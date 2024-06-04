5f8d5d2d6f72ce8fe790dd8bcd8b0c3cb3a477a2
Tirth-Thakkar <tirth1226@gmail.com>
2024-06-03 22:36:56 -0700

    Adding lifelines kmf functionality

--- _data/ApesSurvivorship.py
+++ _data/ApesSurvivorship.py
@@ -1,7 +1,10 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import os
+from lifelines import KaplanMeierFitter
+from lifelines.utils import median_survival_times 
+from lifelines.statistics import logrank_test
 
 def prepare_data(csv_file):
     df = pd.read_csv(csv_file)
@@ -107,18 +110,25 @@ def perform_analysis(csv_file, age_range, separate_types):
     current_dir = os.getcwd()
     output_dir = os.path.join(current_dir, "_data", "CSV_Output", "CSV_Output_Processed")
     output_file_arr = []
+    
     if separate_types:
         data = prepare_data(csv_file)
-        for path in data:
-            # Get the base file name without extension
+        for path in data:    
+            
+            # Get the base file name without extension will be different due to Type Separation
             path_base_name = os.path.splitext(os.path.basename(path))[0]
             output_file = os.path.join(output_dir, f"{path_base_name}_statistics.csv")
-            # Construct the path to the CSV_Data_Processed directory
+            
+            # Construct the path to the CSV_Output_Processed directory
             analyze_deaths(path, output_file, age_range)
             output_file_arr.append(output_file)
     else: 
+        
+        # Getting base file name
         path_base_name = os.path.splitext(os.path.basename(csv_file))[0]
         output_file = os.path.join(output_dir, f"{path_base_name}_statistics.csv")
+        
+        # Output to the CSV_Output Directory
         analyze_deaths(csv_file, output_file, age_range)
         output_file_arr.append(output_file)
 
@@ -141,24 +151,50 @@ def plot_curve(csv_file, title):
     age_range = df['Age Range']
     surviving_members = df['Surviving Members']
     plt.figure()
-    plt.rcParams["figure.autolayout"] = True
     plt.xlabel('Age Range')
     plt.ylabel('Surviving Members')
     plt.title(f'Survivorship Curve {title}')
     plt.plot(age_range, surviving_members, marker='.')
+    
     # If You Want to Show the Plot  
     # plt.show()
+    
     plt.savefig(output_path)  
     plt.close()
 
-    # Wether the survivor ship curve is Type 1, Type 2, or Type 3
-    # Different Types of Survivorship Curves and their meaning     
-    def determinePlotType():
-        raise NotImplementedError
 
-    # Raising wether a species is k-selected (logistic growth) or r-selected (exponential growth)
-    def determineSpeciesGrowthType():
-        raise NotImplementedError
+
+# If we wanted to expand beyond what you would see in APES and would be more realistic towards the actual
+# world we would use the elements that are covered in the fields of Survival Analysis 
+# There may be some calculus involved to determine function types either Type 1, 2, or 3 Survivorship Curves  
+# Learn more https://en.wikipedia.org/wiki/Survivorship_curve 
+
+def survival_analysis(csv_file):
+    kaplan_meier_estimator = KaplanMeierFitter()
+    df = pd.read_csv(csv_file)
+    
+    if "Surviving Members" not in df.columns:
+        raise ValueError("Column 'Surviving Members' not found in the CSV file")
+    
+    # Creating a new DataFrame for duration or the life that each individual lived for 
+    # Based on the amount of deaths in the age range adds the Age Range they died in to the DataFrame 
+    duration = df['Age Range'].repeat(df['Deaths in Range'])
+
+    # Using the fit function from the Lifelines library to the fit the Kaplan Meier Estimator Model to the Dataset
+    # This is similar to what you may have seen scikit-learn's fit or prediction functionality
+    kaplan_meier_estimator.fit(duration)
+    
+    # Getting the file name of the CSV File Passed in
+    current_dir = os.getcwd()
+    output_dir = os.path.join(current_dir, "_data", "CSV_Output", "CSV_Output_Processed")
+    file_base_name = os.path.splitext(os.path.basename(csv_file))[0]
+    output_path = os.path.join(output_dir, f"{file_base_name}_KMF_Survival_Function.png")
+    
+    # Plotting and saving the surivival function
+    kaplan_meier_estimator.survival_function_.plot()
+    plt.xlabel('Age Range')
+    plt.ylabel('Survivorship Probability') 
+    plt.savefig(output_path)
 
 
 if __name__ == '__main__':
@@ -177,21 +213,22 @@ if __name__ == '__main__':
     # Data Source for Dog Data https://rvc-repository.worktribe.com/output/1558210 
     actuarial_data_dogs= os.path.join(data_dir, "dog_life_expectancy.csv")
 
-    # prepare_data(dog_life_expectancy) 
+    # A narrower age range allows for more detailed analysis, a wider one for a smoother less accurate curve 
     cent_19th_output_arr = perform_analysis(csv_file = actuarial_data_19thC, age_range = 5, separate_types = True)
     cent_20th_output_arr = perform_analysis(csv_file = actuarial_data_20thC, age_range = 5, separate_types = True)
     
-
     # Recommended to not separate_types for dogs due to many different breeds in the dataset creating too many files 
     # Unless you like 200+ files in your directory =)
     dog_path_output_arr = perform_analysis(csv_file = actuarial_data_dogs, age_range = 1, separate_types = False)
-
+    
     for path in dog_path_output_arr:
+        survival_analysis(path)
         plot_curve(path, "Dog Life Expectancy")
 
     for path in cent_19th_output_arr:
+        survival_analysis(path)
         plot_curve(path, "19th Century Actuarial Data")
 
     for path in cent_20th_output_arr:
         plot_curve(path, "20th Century Actuarial Data")
-    
+        
